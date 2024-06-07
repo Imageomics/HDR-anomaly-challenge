e1aabc716e27b46d705fcf51d7c850a91b130ffe
egrace479 <e.campolongo479@gmail.com>
2024-06-07 18:29:29 -0400

    Update species A scoring program get proper input files to read, not just directories print all scores and output challenge scores to scores.json for CodaBench to read scores still need proper labels and requirements file is temporary fix pending base container

--- scoring_program_A/score_maj_min.py
+++ scoring_program_A/score_maj_min.py
@@ -6,23 +6,31 @@ import os
 import pathlib
 import sys
 
+# Added to address lack of base container, can remove once created
+from sys import executable
+import subprocess
+subprocess.check_call([executable, "-m", "pip", "install", "-r", str(pathlib.Path(__file__).parent.resolve() / "requirements.txt")])
+
 import numpy as np
 from sklearn.metrics import recall_score, precision_score, f1_score, roc_auc_score, accuracy_score
 
 # Constants
-DEFAULT_CONFIG = pathlib.Path(__file__).parent.resolve() / "scoring_config.yaml"
-HELPER_DIRECTORY = str(pathlib.Path(__file__).parent.parent.resolve() / "helper_scripts")
+#DEFAULT_CONFIG = pathlib.Path(__file__).parent.resolve() / "scoring_config.yaml"
+HELPER_DIRECTORY = str(pathlib.Path(__file__).parent.resolve() / "helper_scripts")
 
 # Importing functions from helper_scripts
 sys.path.append(HELPER_DIRECTORY)
-from dataio import parse_yaml_file, parse_prediction_file, parse_solution_file, parse_major_minor_file, write_yaml_file
+from dataio import parse_prediction_file, parse_solution_file, parse_major_minor_file, save_scores, parse_yaml_file, write_yaml_file
 
+'''
+# This is for local testing only, along with DEFAULT_CONFIG defined above
 def get_config():
     config_path = DEFAULT_CONFIG
     if len(sys.argv) > 1:
-        config_path = argv[1]
+        config_path = sys.argv[1]
         
     return parse_yaml_file(config_path)
+'''
 
 def evaluate_prediction(scores, labels, reversed=False):
     combined = list(zip(scores, labels))
@@ -58,20 +66,20 @@ def evaluate(scores, labels, reversed=False):
 
     return h_recall, h_precision, f1, roc_auc, acc
 
-def evaluate_major_minor_prediction(scores, labels, camids, major_cams, minor_cams, reversed=False):
+def evaluate_major_minor_prediction(scores, labels, filenames, major_cams, minor_cams, reversed=False):
     
     preds, gt = evaluate_prediction(scores, labels, reversed=reversed)
-    tmp = list(zip(scores, labels, camids))
+    tmp = list(zip(scores, labels, filenames))
     tmp = sorted(tmp, key=lambda x: x[0], reverse=reversed)
-    sorted_camids = np.array(tmp)[:, 2]
-    major_idx = np.isin(np.array(sorted_camids), major_cams)
-    minor_idx = np.isin(np.array(sorted_camids), minor_cams)
+    sorted_filenames = np.array(tmp)[:, 2]
+    major_idx = np.isin(np.array(sorted_filenames), major_cams)
+    minor_idx = np.isin(np.array(sorted_filenames), minor_cams)
     maj_acc = accuracy_score(gt[major_idx], preds[major_idx])
     min_acc = accuracy_score(gt[minor_idx], preds[minor_idx])
     
     return maj_acc, min_acc
 
-def score_predictions(pred_camids, pred_vals, sol_gt_aligned, reverse_score_prediction=False, mm_vals_aligned=None):
+def score_predictions(pred_vals, sol_gt_aligned, mm_vals_aligned=None, reverse_score_prediction=False):
     h_recall, h_precision, f1, roc_auc, acc = evaluate(pred_vals, sol_gt_aligned, reversed=reverse_score_prediction)
     
     scores = {
@@ -88,41 +96,89 @@ def score_predictions(pred_camids, pred_vals, sol_gt_aligned, reverse_score_pred
         # Call evaluate_major_minor_prediction and add to scores
         # NOTE: will have to handle incomplete list (for example: the mm_vals_aligned will not match the shape of all pred_vals)
         # it's a subset
+        # Full CSV labeled: 
+        # the reference csv file has column `ssp_indicator` with values `major` and `minor` for all entries
         #evaluate_major_minor_prediction()
+        # scores should have major_recall, minor_recall, and roc_auc (from above)
         
     return scores
 
 if __name__ == "__main__":
+    # Get scoring configurations -- for local testing
+    #config = get_config()
+    
+    # Directory to read labels from
+    input_dir = sys.argv[1]
+    solutions = os.path.join(input_dir, 'ref')
+    prediction_dir = os.path.join(input_dir, 'res')
 
-    # Get scoring configurations
-    config = get_config()
+
+    print("Using input_dir: " + input_dir)
+    print("Using solutions: " + solutions)
+    print("Using prediction_dir: " + prediction_dir)
+
+    # Directory to output computed score into
+    output_dir = sys.argv[2]
+    print("Using output_dir: " + output_dir)
+
+    prediction_file = os.path.join(prediction_dir,'predictions.txt')
+
+    # Check if file exists
+    if not os.path.isfile(prediction_file):
+        print('[-] Test prediction file not found!')
+        print(prediction_file)
+        sys.exit("Couldn't read predictions")
     
     # Get predictions
-    pred_camids, pred_vals = parse_prediction_file(config.input_data)
+    pred_filenames, pred_vals = parse_prediction_file(prediction_file)
     
     # Get solutions
-    sol_camids, sol_gt = parse_solution_file(config.solution_data)
+    file_list = os.listdir(solutions)
+    print(f"files in {solutions}: {file_list}")
+    # Check if file exists --- should generally be file_list[0], there's a '__MACOSX' showing up from zipping
+    if "val_A.csv" in file_list:
+        solution_file = os.path.join(solutions,'val_A.csv')
+        sol_filenames, sol_gt = parse_solution_file(solution_file)
+        print("got solutions with 'val_A.csv'")
+    elif "test_A.csv" in file_list:
+        solution_file = os.path.join(solutions,'test_A.csv')
+        sol_filenames, sol_gt = parse_solution_file(solution_file)
+        print("got solutions with 'test_A.csv'")
+    elif len(file_list) > 0:
+        solution_file = os.path.join(solutions, file_list[0])
+        sol_filenames, sol_gt = parse_solution_file(solution_file)
+        print(f"got solutions with {file_list[0]}")
+    else:
+        sys.exit(f"Couldn't find solution file, have {len(file_list)} files in {solutions}")
+
+    # Align predictions with solution via filename
+    def align(ref_filenames, ref_vals):
+        return [ref_vals[ref_filenames.index(filename)] for filename in pred_filenames]
+    sol_filenames, sol_gt
+    sol_gt_aligned = align(sol_filenames, sol_gt)
     
-    # Align predictions with solution via CAMID
-    def align(ref_camids, ref_vals):
-        return [ref_vals[ref_camids.index(camid)] for camid in pred_camids]
-    sol_camids, sol_gt
-    sol_gt_aligned = align(sol_camids, sol_gt)
-    
+
+    ## TODO: This needs re-write
     # Optionally get major-minor list
+    # the reference csv file has column `ssp_indicator` with values `major` and `minor`
     mm_vals_aligned = None
+    '''
     if hasattr(config, 'major_minor_data') and config.major_minor_data:
         report_major_minor_score = True
-        mm_camids, mm_vals = parse_major_minor_file(config.major_minor_data)
-        mm_vals_aligned = align(mm_camids, mm_vals)
-        
+        mm_filenames, mm_vals = parse_major_minor_file(config.major_minor_data)
+        mm_vals_aligned = align(mm_filenames, mm_vals)
+    '''
+    ## End part needing re-write
+
+
     # Get scores
-    scores = score_predictions(pred_camids, pred_vals, sol_gt_aligned, config.reverse_score_prediction, mm_vals_aligned)
-    print(scores)
+    scores = score_predictions(pred_vals, sol_gt_aligned, mm_vals_aligned) #, config.reverse_score_prediction)
+    print(f"Full Scores: {scores}")
     
     # Create ouput directory
-    os.makedirs(pathlib.Path(config.output_data).parent.resolve(), exist_ok=True)
-    
-    # Write scores
-    write_yaml_file(config.output_data, scores)
+    os.makedirs(pathlib.Path(output_dir).parent.resolve(), exist_ok=True)    
+    output_file = os.path.join(output_dir, "scores.json")
 
+    # Write scores -- yaml probably unnecessary
+    write_yaml_file(output_file, scores)
+    save_scores(output_file, scores)
